64058699117f9d8bd56ec31556e6386c395e4706
Евгений Клюев <craftycoderpro@gmail.com>
2022-04-04 13:02:38 +0300

    styles & html main page

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Header } from './components/Header'
+import { Categories } from './components/Categories'
+import { Sort } from './components/Sort'
+import { ProductItem } from './components/ProductItem'
+
+import './scss/app.scss'
+
+function App() {
+   return (
+      <div className="wrapper">
+         <Header />
+
+         <div className="content">
+            <div className="container">
+               <div className="content__top">
+                  <Categories />
+                  <Sort />
+               </div>
+
+               <h2 className="content__title">Все пиццы</h2>
+
+               <div className="content__items">
+                  <ProductItem title="Чизбургер-пицца" price={395} />
+               </div>
+            </div>
+         </div>
+      </div>
+   )
+}
+
+export default App
