e1e5177e19206a83f21d5d3ed66e3181dc4f8630
Евгений Клюев <craftycoderpro@gmail.com>
2022-04-07 08:06:46 +0300

    styling & router6.3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,10 +10,11 @@ export function Home() {
       fetch('https://63481a210484786c6e91d7a6.mockapi.io/items')
          .then(res => res.json())
          .then(data => setProducts(data))
+      window.scrollTo(0, 0)
    }, [])
 
    return (
-      <>
+      <div className="container">
          <div className="content__top">
             <Categories />
             <Sort />
@@ -26,6 +27,6 @@ export function Home() {
                ? products.map(item => <ProductItem key={item.id} {...item} />)
                : [...Array(6)].map((_, i) => <Skeleton key={i} className="pizza-block" />)}
          </div>
-      </>
+      </div>
    )
 }
